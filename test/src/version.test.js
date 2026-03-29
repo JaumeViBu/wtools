1aa265b0aed53705ecb286bef11ce9eba0459429
jaume vidal <jaumevibu@gmail.com>
2026-03-29 16:03:53 +0200

    fix version test
    remove magic string and use only the package.json version

--- test/src/version.test.js
+++ test/src/version.test.js
@@ -3,12 +3,9 @@ import version from '../../src/commands/version.js';
 import { version as pkgVersion } from '../../package.json' with { type: 'json' };
 
 describe('version', () => {
-  it('should return the version', () => {
-    expect(version()).toBe('0.0.0');
-  });
+    it('should match package.json version', () => {
+        expect(version()).toBe(pkgVersion);
+    });
 });
 
 
-it('should match package.json version', () => {
-  expect(version()).toBe(pkgVersion);
-});
